4894f564c02c175fe6bda7071ee7bb1a10ce89aa
Antonio <rodrigowserra@gmail.com>
2024-07-30 19:56:31 +0100

    Fix - Ajuste no nome da variável static final

--- src/main/java/bank/constantstexts/Constants.java
+++ src/main/java/bank/constantstexts/Constants.java
@@ -2,6 +2,6 @@ package bank.constantstexts;
 
 public class Constants {
  public static final String inexiste = "Objeto Inexiste";
- public static final String notfound = "Objeto não encontrado";
+ public static final String NOT_FOUND = "Objeto não encontrado";
  public static final String existente = "Objeto já existe";
 }
